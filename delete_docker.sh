b6b560a4d8cbbe4564123a787a6d3a38e168de90
Arthur Savage <alerc@meta.ua>
2020-10-13 17:38:32 +0300

    Update delete_docker.sh

--- delete_docker.sh
+++ delete_docker.sh
@@ -17,3 +17,43 @@ sudo rm -rf /var/lib/docker /etc/docker
 sudo rm /etc/apparmor.d/docker
 sudo groupdel docker
 sudo rm -rf /var/run/docker.sock
+
+# https://howtoinstall.co/en/ubuntu/xenial/docker?action=remove
+echo Uninstall docker...
+#To remove just docker package itself from Ubuntu 16.04 (Xenial Xerus) execute on terminal:
+
+sudo apt-get remove docker
+
+echo Uninstall docker and it's dependent packages
+#sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce
+#To remove the docker package and any other dependant package which are no longer needed from Ubuntu Xenial.
+
+sudo apt-get autoremove docker
+
+echo Purging docker...
+
+#If you also want to delete configuration and/or data files of docker from Ubuntu Xenial then this will work:
+
+sudo apt-get purge docker
+
+echo To delete configuration and/or data files of docker and it's dependencies from Ubuntu Xenial then execute:
+sudo apt-get autoremove --purge docker  
+
+
+Удалить из загрузки
+# https://askubuntu.ru/questions/310531/otkljuchit-avtozapusk-docker-pri-zagruzke
+
+# Отключение при загрузке
+#https://askubuntu.ru/questions/310531/otkljuchit-avtozapusk-docker-pri-zagruzke
+# https://qastack.ru/ubuntu/766318/disable-docker-autostart-at-boot
+sudo systemctl disable docker.service
+sudo systemctl disable docker.socket
+
+# удалить из загрузки
+sudo systemctl kill docker.service
+sudo systemctl kill docker.socket
+
+
+# Просомтр процесса
+# systemctl list-unit-files | grep -i docker
+# ps axf | grep docker  
